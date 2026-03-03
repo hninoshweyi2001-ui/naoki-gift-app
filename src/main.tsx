0c31ce3eb6d58fa1a43379188564b2801ead35ca
hninoshweyi2001-ui <hninoshweyi2001@gmail.com>
2026-03-03 21:38:01 +0630

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// ဖိုင်လမ်းကြောင်းကို components/App/App ထဲအထိ ပြောင်းလိုက်ပါတယ်
-import App from './components/App/App' 
+import App from './components/App/App' // 👈 ဒီလမ်းကြောင်း မှန်ပါစေ
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
