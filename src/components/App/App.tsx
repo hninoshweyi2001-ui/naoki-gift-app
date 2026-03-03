527a33df6f358a3e693b818d41285adc4a52e0fd
hninoshweyi2001-ui <hninoshweyi2001@gmail.com>
2026-03-03 17:07:52 +0630

    Update App.tsx

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,28 +6,23 @@ import { useShowPopup } from "@vkruglikov/react-telegram-web-app";
 export type AppProps = {
   className?: string;
 };
+
 export const App: React.FC<AppProps> = ({ className }) => {
   const showPopup = useShowPopup();
 
   const showPopupOnClick = async () => {
-    const message =
-      "Thanks for using react-mini-app! I hope it helps you to create awesome Telegram Mini apps!";
-    await showPopup({ title: "Hey!", message: message });
+    await showPopup({ 
+      title: "Naoki Gift", 
+      message: "Welcome to my Rocket Game!" 
+    });
   };
 
   return (
     <div className={clsx(styles.App, className)}>
       <div className={styles.container}>
-        <a
-          href="https://github.com/usernein/react-mini-app"
-          target="_blank"
-          className={styles.githubLink}
-        >
-          <h1 className={styles.title}>react-mini-app</h1>
-        </a>
-
-        <button onClick={showPopupOnClick} className={styles.clickMe}>
-          Show popup!
+        <h1>Naoki Rocket Game</h1>
+        <button className={styles.button} onClick={showPopupOnClick}>
+          Start Game
         </button>
       </div>
     </div>
